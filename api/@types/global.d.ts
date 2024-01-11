136aa618c04416ca7c1f149319b57678136e433f
Ari <arifilhosilva235@gmail.com>
2024-01-11 10:51:50 -0300

    feat: adiciona interface de escopo global

---
+++ api/@types/global.d.ts
@@ -0,0 +1,18 @@
+import { User } from "@prisma/client";
+
+declare global {
+    namespace Express {
+        interface Request {
+            user: User;
+        }
+    }
+}
+
+namespace NodeJS {
+    interface ProcessEnv {
+        APP_URL: string,
+        PORT: number,
+        SECRET_KEY: string,
+        JWT_EXPIRATION: string,
+    }
+}
