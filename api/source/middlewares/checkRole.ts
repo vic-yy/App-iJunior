aec7d2c1883eef965b6e46b8ff9a1209b2017f88
Ari <arifilhosilva235@gmail.com>
2024-01-11 12:01:01 -0300

    fix: corrige para config automática do eslint

--- api/source/middlewares/checkRole.ts
+++ api/source/middlewares/checkRole.ts
@@ -1,15 +1,15 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import statusCodes from '../../utils/statusCodes';
 import { NotAuthorizedError } from '../../errors/NotAuthorizedError';
 
-function checkRole(req: Request, res: Response, next: NextFunction, role: string){
+function checkRole(req: Request, res: Response, next: NextFunction, role: string) {
 
-    const roleUser = req.user.role
-    if(roleUser === role){
-        next()
-    }
-    res.status(statusCodes.UNAUTHORIZED).send({msg: 'Não autorizado'});
-    throw new NotAuthorizedError('Usuário sem permissão')
+	const roleUser = req.user.role;
+	if (roleUser === role) {
+		next();
+	}
+	res.status(statusCodes.UNAUTHORIZED).send({ msg: 'Não autorizado' });
+	throw new NotAuthorizedError('Usuário sem permissão');
 }
 
 export default checkRole;
