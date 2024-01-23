dfe7ae536abc7ed1aebe24919af0dabba277fa56
diegochagas30 <140138148+diegochagas30@users.noreply.github.com>
2024-01-23 02:13:22 -0300

    feat: pequenas atualizações e instalação de bibliotecas para estilização

--- client/App.tsx
+++ client/App.tsx
@@ -1,15 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import theme from './src/global/styles/theme';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +13,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text>Open up App.js to start working on your app!</Text>
+    </View>
+    </ThemeProvider>
+  );
+}
+
