d5f39659147f9353321dfe7f8d1c8714d75ac30c
Luísa Gontijo <luisaporf@gmail.com>
2024-02-02 13:48:46 -0300

    fix: emaill pattern

--- api/utils/isEmailValid.ts
+++ api/utils/isEmailValid.ts
@@ -1,6 +1,6 @@
 export function isEmailValid(email:string) {
-	const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-z][a-zA-Z0-9.-]*\.[a-zA-Z]{2,}$/;
 	return emailPattern.test(email);
 }
 
-export default isEmailValid;
+export default isEmailValid;	
